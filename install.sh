7b4df67748bce2f24387e4c4380aecb4dd7ef9c4
Henrik Nelson <henrik@cliffords.nu>
2020-04-21 05:59:41 +0200

    Updated fd to 8.0.0

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ REPLACE="
 print_modname() {
   ui_print "*********************************************"
   ui_print "     fd for Android                          "
-  ui_print "         - v 7.5.0                           "
+  ui_print "         - v 8.0.0                           "
   ui_print "         - built by nelshh @ xda-developers  "
   ui_print "*********************************************"
 }
